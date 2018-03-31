a9dbf8c6e86953c6d3df7357c97caf247025fade
kmasz <tomasz.nowak.kmasz@gmail.com>
2018-03-31 12:09:21 +0200

    nawet wiecej niz MVP -potrzebne porzadki i css

--- testy/db_test_list_tables.py
+++ testy/db_test_list_tables.py
@@ -8,14 +8,28 @@ import plotly
 import plotly.graph_objs as go
 import sqlite3
 import pandas as pd
+import datetime
+import time
 
 conn = sqlite3.connect('../cryptocurrency.db')
+df2 = pd.read_sql("SELECT * FROM bitcoin ORDER BY last_updated DESC", conn)
 #df1 = pd.read_sql("SELECT name FROM sqlite_master WHERE type='table'", conn)
 #available_cryptocurrency = df1['Name']
 #print (df1['name'])
-available_crypto = u"0x"
-query = "SELECT * FROM " + available_crypto + " ORDER BY last_updated DESC LIMIT 1"
+available_crypto = "bitcoin"
+date_value = df2['last_updated']
+query = "SELECT * FROM " + available_crypto + " ORDER BY last_updated DESC"# LIMIT " + str(date_value)
 df = pd.read_sql(query, conn)
 #df = pd.read_sql("SELECT * FROM ? ORDER BY ? DESC LIMIT ?", conn, params=[available_crypto, 'last_updated', 4])
 #df = pd.read_sql("SELECT * FROM sentiment WHERE tweet LIKE %s ORDER BY unix DESC LIMIT 1000", conn ,params=[sentiment_term,])
-print(df)
+#print(df)
+#print(df['last_updated'][::100].unique())
+
+#X = df.index[-date_value:]
+dff = df[df['last_updated'] > 1521985467]
+
+print(dff)
+#for date in df['last_updated'][::100].unique():
+#    print(datetime.datetime.fromtimestamp(date).strftime('%m-%d %H:%M'))
+    #print(int(date))
+
